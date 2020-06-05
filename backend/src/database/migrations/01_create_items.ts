5db38a20ba76cb352733f1fc500b2f7649eb9db1
votanruchel <votanruchel@hotmail.com>
2020-06-05 15:39:31 -0300

    Adding Server and Web React Frontend

---
+++ backend/src/database/migrations/01_create_items.ts
@@ -0,0 +1,17 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    //CREATE TABLE
+   return knex.schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    })
+}
+
+export async function down(knex: Knex) {
+
+    //ROLLBACK
+    return knex.schema.dropTable('items')
+
+}
